86ffd69588f0df2fa2d5ddf601a129787f409d84
inul74 <zmusthofa74@gmail.com>
2024-12-08 07:13:42 +0700

    margin ruler component

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -7,6 +7,8 @@ import { useEditorStore } from "@/store/use-editor-store";
 import { FontSizeExtension } from "@/extensions/font-size";
 import { LineHeightExtension } from "@/extensions/line-height";
 
+import { Ruler } from "./ruler";
+
 const TipTapEditor = dynamic(
   async () => {
     const [
@@ -134,6 +136,7 @@ const TipTapEditor = dynamic(
       });
       return (
         <div className="size-full overflow-x-auto bg-[#F9FBFD] px-4 print:p-0 print:bg-white print:overflow-visible">
+          <Ruler />
           <div className="min-w-max flex justify-center w-[816px] py-4 print:py-0 mx-auto print:w-full print:min-w-0">
             <EditorContent editor={editor} />
           </div>
