46d019d97dc547fc8d69de8313428079fec1657b
inul74 <zmusthofa74@gmail.com>
2024-12-09 23:07:45 +0700

    search input component

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Link from "next/link";
+import { Navbar } from "./navbar";
+
+const Home = () => {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <div className="fixed top-0 left-0 right-0 z-10 h-16 bg-white p-4">
+        <Navbar />
+      </div>
+      <div className="mt-16">
+        Click
+        <Link href="/documents/123">
+          <span className="underline text-blue-500 mx-1">here</span>
+        </Link>
+        to goto document id
+      </div>
+    </div>
+  );
+};
+
+export default Home;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,14 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { Inter } from "next/font/google";
+import { NuqsAdapter } from "nuqs/adapters/next/app";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const inter = Inter({
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: "inText | inuldev",
+  title: "inText | inulDev",
   description: "inuldev Text editor workspace",
   icons: [
     {
@@ -32,10 +26,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={inter.className}>
+        <NuqsAdapter>{children}</NuqsAdapter>
       </body>
     </html>
   );
