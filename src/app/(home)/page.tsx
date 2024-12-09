9f3fb297f7cfeaa7b3ea3f71258b1d174559223b
inul74 <zmusthofa74@gmail.com>
2024-12-10 00:06:32 +0700

    templates gallery component

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,6 +1,5 @@
-import React from "react";
-import Link from "next/link";
 import { Navbar } from "./navbar";
+import { TemplatesGallery } from "./templates-gallery";
 
 const Home = () => {
   return (
@@ -9,11 +8,7 @@ const Home = () => {
         <Navbar />
       </div>
       <div className="mt-16">
-        Click
-        <Link href="/documents/123">
-          <span className="underline text-blue-500 mx-1">here</span>
-        </Link>
-        to goto document id
+        <TemplatesGallery />
       </div>
     </div>
   );
