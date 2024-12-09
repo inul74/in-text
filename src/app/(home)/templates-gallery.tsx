543fe66c30258d466cb739242310a64c37e04a36
inul74 <zmusthofa74@gmail.com>
2024-12-10 05:56:27 +0700

    create & list documents

--- src/app/(home)/templates-gallery.tsx
+++ src/app/(home)/templates-gallery.tsx
@@ -1,5 +1,9 @@
 "use client";
 
+import { useState } from "react";
+import { useMutation } from "convex/react";
+import { useRouter } from "next/navigation";
+
 import {
   Carousel,
   CarouselContent,
@@ -7,11 +11,28 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel";
+
 import { cn } from "@/lib/utils";
 import { templates } from "@/constants/templates";
 
+import { api } from "../../../convex/_generated/api";
+
 export const TemplatesGallery = () => {
-  const isCreating = false;
+  const router = useRouter();
+  const create = useMutation(api.documents.create);
+  const [isCreating, setIsCreating] = useState(false);
+
+  const onTemplateClick = (title: string, initialContent: string) => {
+    setIsCreating(true);
+    create({ title, initialContent })
+      .then((documentId) => {
+        router.push(`/documents/${documentId}`);
+      })
+      .finally(() => {
+        setIsCreating(false);
+      });
+  };
+
   return (
     <div className="bg-[#F2F3F4]">
       <div className="max-w-screen-xl mx-auto px-16 py-6 flex flex-col gap-y-4">
@@ -31,7 +52,8 @@ export const TemplatesGallery = () => {
                 >
                   <button
                     disabled={isCreating}
-                    onClick={() => {}}
+                    // TODO: Add proper initial content
+                    onClick={() => onTemplateClick(template.label, "")}
                     style={{
                       backgroundImage: `url(${template.imageUrl})`,
                       backgroundSize: "cover",
